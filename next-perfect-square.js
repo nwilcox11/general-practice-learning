b8b26fe75df543d319f5cf4998a392555b94f115
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-05-18 19:16:42 -0400

    quick refactor

--- next-perfect-square.js
+++ next-perfect-square.js
@@ -13,7 +13,6 @@ You may assume the parameter is positive.
 // http://betterexplained.com/articles/surprising-patterns-in-the-square-numbers-1-4-9-16/
 
 function findNextSquare(sq) {
-
   if(Math.floor(Math.sqrt(sq)) * Math.floor(Math.sqrt(sq)) === sq) {
     let square = Math.sqrt(sq);
     let difference = (square * 2) + 1;
@@ -23,7 +22,24 @@ function findNextSquare(sq) {
   } else {
     return -1;
   }
-
 }
 
-console.log(findNextSquare());
+// quick refactor
+
+// Because we are checking for a perfect square, we can eliminate the expensive, wrapping if statement.
+// Check if the square root of the input is a whole number, does it divide evenly by one?
+// We then know the next perfect square will be the ^2 of sq + 1.
+// I avoided Math.pow(sq + 1, 2);  Not sure if (sq + 1) * (sq * 1) is more efficient or not
+
+function findNextSquare(sq) {
+  sq = Math.sqrt(sq);
+
+  return sq % 1 === 0 ? (sq + 1) * (sq + 1) : -1;
+}
+
+
+
+console.log(findNextSquare(121)); // => 144
+console.log(findNextSquare(114)); // => -1, 114 is not perfect
+console.log(findNextSquare(625)); // => 676
+console.log(findNextSquare(319225)); // => 320356
