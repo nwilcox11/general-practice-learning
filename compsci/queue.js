82b7b0f7752b7d5f6dae9265e1a68beee28ab65a
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-09-25 08:29:53 -0400

    creates queue skeleton

---
+++ compsci/queue.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+  First In, First Out
+  First elememnt added will be the first element removed.
+*/
+
+class Queue {
+  constructor() {
+    this.size = 0;
+    this.storage = {};
+  }
+  add() {
+
+  }
+  remove() {
+    
+  }
+}
