9ffc5470fcbffbf988600cdab0976a5e6bab7d53
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-03-06 06:59:50 -0500

    removes buggy implementation

--- redditProblems/binarySearchRotated.js
+++ redditProblems/binarySearchRotated.js
@@ -2,6 +2,9 @@ const assert = require('assert');
 const rotate = require('../rotatingArray');
 
 const test = [5,6,7,8,1,2,3];
+const test2 = rotate([1,2,3,4,5,6,7,8,9,10,11,12,13]);
+const test3 = [10, 20, 1, 3, 6, 7 ,8];
+const test4 = [10, 20, 40, 3, 6, 7 , 9];
 
 /** Implement binary search in a rotated sorted array with no duplicates (ex. {5,6,7,8,1,2,3}) **/
 
@@ -40,51 +43,6 @@ const rotatedSearch = binarySearcher(test);
 
 assert.equal(rotatedSearch(1), 1);
 assert.equal(rotatedSearch(4), 'Not Found');
-
-const test2 = rotate([1,2,3,4,5,6,7,8,9,10,11,12,13]);
-
-/** O(log n) **/
-
-function binarySearchRotated(target, array) {
-
-    let piv = Math.floor(array.length / 2) + 1; // dont choose the max number as pivot
-    let left = array.slice(0, piv);
-    let right = array.slice(piv);
-
-    /** search through both left and right lists until we find it, this is gross looking **/
-    while(right.length && left.length) {
-        let rMid = Math.floor(right.length / 2);
-        let lMid = Math.floor(left.length / 2);
-
-        if ((right.length === 1 && right[rMid] !== target) && 
-            (left.length === 1 && left[lMid] !== target)) {
-            return 'Not Found';
-        }
-
-        if (right[rMid] === target) {
-            return right[rMid];
-        } else if(right[rMid] > target) {
-            right = right.slice(0, rMid);
-        } else {
-            right = right.slice(rMid, right.length);
-        }
-
-        if (left[lMid] === target) {
-            return left[lMid];
-        } else if (left[lMid] > target) {
-            left = left.slice(0, lMid);
-        } else {
-            left = left.slice(lMid, left.length);
-        }
-    }
-}
-
-assert.equal(binarySearchRotated(1, test2), 1);
-assert.equal(binarySearchRotated(4, test2), 4);
-assert.equal(binarySearchRotated(12, test2), 12);
-assert.equal(binarySearchRotated(7, test2), 7);
-assert.equal(binarySearchRotated(9, test2), 9);
-assert.equal(binarySearchRotated(100, test2), 'Not Found');
 
 /** Better **/
 function rotatedBinSearch(target, array) {
@@ -163,8 +121,6 @@ function recBinSearch(target, left, right, array) {
     }
 }
 
-const test3 = [10, 20, 1, 3, 6, 7 ,8];
-const test4 = [10, 20, 40, 3, 6, 7 , 9];
 assert.equal(recBinSearch(1, 0, test3.length - 1, test3), 1);
 assert.equal(recBinSearch(20,  0, test3.length - 1, test3), 20);
 assert.equal(recBinSearch(12,  0, test2.length - 1,test2), 12);
