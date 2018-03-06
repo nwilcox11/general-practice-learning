82ae0a79742c5ffdfb0abc790dd40f9169607d62
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-03-05 22:12:20 -0500

    rotated recursive binary search

--- redditProblems/binarySearchRotated.js
+++ redditProblems/binarySearchRotated.js
@@ -3,7 +3,7 @@ const rotate = require('../rotatingArray');
 
 const test = [5,6,7,8,1,2,3];
 
-/** Implement binary search in a rotated sorted array (ex. {5,6,7,8,1,2,3}) **/
+/** Implement binary search in a rotated sorted array with no duplicates (ex. {5,6,7,8,1,2,3}) **/
 
 /** 
  *  O(n log n)
@@ -120,3 +120,50 @@ assert.equal(rotatedBinSearch(12, test2), 12);
 assert.equal(rotatedBinSearch(7, test2), 7);
 assert.equal(rotatedBinSearch(9, test2), 9);
 assert.equal(rotatedBinSearch(100, test2), 'Not Found');
+
+/** Recursive rotated binary search -- O(log n) -- Best? **/
+/**
+ * Grab middle element, if its target return
+ * Check to see if left half is sorted, once we know which half is sorted we can check which half target is in
+ *      check to see which half target is in, binary search in this half
+ * if left half is not sorted, right must be
+ *      check to see which half target is in, call binary search in this half
+ * **/
+function recBinSearch(target, left, right, array) {
+    if (left > right) {
+        return 'Not Found';
+    }
+    /** grabbing middle element **/
+    let mid = Math.floor((right + left) / 2);
+    if (array[mid] === target) {
+        return array[mid];
+        /** checking if left array is sorted **/
+    } else if (array[left] <= array[mid]) {
+        /** if left array is sorted  **/
+        if (target >= array[left] && target <= array[mid]) {
+            /** target is in the left half **/
+            return recBinSearch(target, left, mid - 1, array);
+        } else {
+            /** target is in the right half **/
+            return recBinSearch(target, mid + 1, right, array);
+        }
+        /** if left bound to mid is not sorted, mid to right bound must be sorted **/
+    } else {
+        if (target >= array[mid] && target <= array[right]) {
+            /** target is in the right half **/
+            return recBinSearch(target, mid + 1, right, array);
+        } else {
+            /** target is in the left half **/
+            return recBinSearch(target, left, mid - 1, array);
+        }
+    }
+}
+
+const test3 = [10, 20, 1, 3, 6, 7 ,8];
+const test4 = [10, 20, 40, 3, 6, 7 , 9];
+assert.equal(recBinSearch(1, 0, test3.length - 1, test3), 1);
+assert.equal(recBinSearch(20,  0, test3.length - 1, test3), 20);
+assert.equal(recBinSearch(12,  0, test2.length - 1,test2), 12);
+assert.equal(recBinSearch(7,  0, test3.length - 1, test3), 7);
+assert.equal(recBinSearch(9,  0, test4.length - 1, test4), 9);
+assert.equal(recBinSearch(100,  0, test2.length - 1, test2), 'Not Found');
