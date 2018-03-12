d6375bb3bba8ffce15dfac33fcb22f69c5b07e45
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-03-12 16:39:47 -0400

    binary to Interger with tests

--- redditProblems/binaryInt.js
+++ redditProblems/binaryInt.js
@@ -1,5 +1,25 @@
 const assert = require('assert');
 
-/** return the binary form of an int **/
+/** 
+ * return the binary form of an int 
+ * convert integer to binary, start with the integer in question and 
+ * divide number by 2 keeping the quotient and the remainder. 
+ * Continue dividing the quotient by 2 until you get a quotient of zero. 
+ * Shift the remainders onto and array (could be a string), join and coerce to a number
+ * **/
 
-function intToBinary(n) {}
+function intToBinary(n) {
+    let bin = [];
+    while(n !== 0) {
+        let remainder = n % 2;
+        n = Math.floor(n / 2);
+        bin.unshift(remainder);
+    }
+    return Number(bin.join(''));
+}
+
+assert.equal(intToBinary(100), 1100100);
+assert.equal(intToBinary(2), 10);
+assert.equal(intToBinary(27), 11011);
+assert.equal(intToBinary(8), 1000);
+assert.equal(intToBinary(74), 1001010);
