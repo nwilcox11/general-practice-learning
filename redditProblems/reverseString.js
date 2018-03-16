7aa65c27e4bab7cd087ca9215fbe286afa0bb824
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-03-15 20:33:17 -0400

    recursive and a sneaky reduce right

--- redditProblems/reverseString.js
+++ redditProblems/reverseString.js
@@ -15,3 +15,24 @@ function reverse(string) {
 
 assert.equal(reverse('nick'), 'kcin');
 assert.equal(reverse('hey im nick'), 'kcin mi yeh');
+
+/**
+ * Recursive reverse
+ * **/
+
+function recusiveReverse(string) {
+    return string ? recusiveReverse(string.substr(1)) + string[0] : string;
+}
+
+assert.equal(recusiveReverse('nick'), 'kcin');
+assert.equal(recusiveReverse('hey im nick'), 'kcin mi yeh');
+
+/**
+ * Sneaky rightReduce for the win
+ * **/
+
+function reduceRightRev(string) {
+    return string.split('').reduceRight((str, curr) => str += curr, '');
+}
+assert.equal(reduceRightRev('nick'), 'kcin');
+assert.equal(reduceRightRev('hey im nick'), 'kcin mi yeh');
