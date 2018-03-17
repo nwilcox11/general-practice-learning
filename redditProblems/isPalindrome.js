681acf227cdb15e86ef765980afb9a1c9c04a80d
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-03-17 11:22:49 -0400

    adds comments for next implemtations

--- redditProblems/isPalindrome.js
+++ redditProblems/isPalindrome.js
@@ -5,6 +5,8 @@ const assert = require('assert');
  * a word, phrase, or sequence that reads the same backward as forward
  * **/
 
+/** start at each end and check each char 
+ * O(n) **/
 function isPalindrome(string) {
     let normalized = string.replace(/\s/g, '').toLowerCase(); // removes casing and spaces
     let start = 0;
@@ -27,7 +29,10 @@ assert.equal(isPalindrome('nick'), false);
 assert.equal(isPalindrome('wnickw'), false);
 assert.equal(isPalindrome('Al lets Della call Ed Stella'), true);
 assert.equal(isPalindrome('Ah Satan sees Natasha'), true);
-
 /**
  * recursive next
  * **/
+
+ /**
+  * O(n/2)
+  * **/
