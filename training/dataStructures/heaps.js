71cb2df0c10b630bb1dfc661404c1d767cfdf9f6
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-03-20 21:45:37 -0400

    initial heaps

---
+++ training/dataStructures/heaps.js
@@ -0,0 +1,15 @@
+/** 
+ * Implementation of a priority queue
+ * An array visualized as a nearly complete binary tree
+ * **/
+
+/** returns max heap from unordered array **/
+function buildMaxHeap() { }
+/** perform one 'swap' or correction of a heap property **/
+function maxHeapify() { }
+/** returns and removes the max value of the max heap **/
+function extractMax() { }
+/** returns a sorted array using heap sort O(n log n) **/
+function heapSort() { }
+/** insert into the priority queue **/
+function insert() { }
