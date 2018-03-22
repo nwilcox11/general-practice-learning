fb606af0186ec00d647d59a657f9225023bc8313
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-03-21 21:28:17 -0400

    initial heapify implementation -- can be cleaned up

--- training/dataStructures/heaps.js
+++ training/dataStructures/heaps.js
@@ -1,15 +1,47 @@
 /** 
  * Implementation of a priority queue
  * An array visualized as a nearly complete binary tree
+ * 
+ * Max Heap: Each Parent is greater than or equal to each of its children
  * **/
+
+// left child = 2i + 1
+// right child = 2i + 2
+
+const test = [16,8,10,14,7,9,3,2,4,1];
 
 /** returns max heap from unordered array **/
 function buildMaxHeap() { }
 /** perform one 'swap' or correction of a heap property **/
-function maxHeapify() { }
+function maxHeapify(array, index) {
+    let largest = index; // set largest to index
+    let leftIndex = (2 * index) + 1;
+    let rightIndex = (2 * index) + 2;
+    let size = array.length - 1;
+    let temp;
+    /** if left index is in heap and element at left is > than element at root/index **/
+    if (leftIndex <= size && array[leftIndex] > array[largest]) {
+        largest = leftIndex;
+    }
+    /** check if right child is larger that current largest **/
+    if (rightIndex <= size && array[rightIndex] > array[largest]) {
+        largest = rightIndex;
+    }
+    /** if largest is not the index -- swap index and largest **/
+    if (largest !== index) {
+        temp = array[index];
+        array[index] = array[largest];
+        array[largest] = temp;
+        /** heapify the next subTree with largest at root/index **/
+        maxHeapify(array, largest);
+    }
+    return array;
+}
 /** returns and removes the max value of the max heap **/
 function extractMax() { }
 /** returns a sorted array using heap sort O(n log n) **/
 function heapSort() { }
 /** insert into the priority queue **/
 function insert() { }
+
+console.log(maxHeapify(test, 1));
