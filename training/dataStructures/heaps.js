73da2cef536a2e967e4ed64b0e52391afe729aa2
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-03-26 22:46:01 -0400

    adds class implementation of maxHeap, operates on shared array state

--- training/dataStructures/heaps.js
+++ training/dataStructures/heaps.js
@@ -10,7 +10,7 @@
 // parent = Math.floor(i - 1) / 2
 
 const test = [16,8,10,14,7,9,3,2,4,1];
-const test3 = [11,13,5,7,6,12,17,19,28,100,29]
+
 /** returns max heap from unordered array, does not modify original array **/
 function buildMaxHeap(array) {
     let maxHeap = [...array];
@@ -75,7 +75,6 @@ function insert(maxHeap, node) {
     /** add node to left most available position **/
     maxHeap.push(node);
     let start = Math.floor((maxHeap.length - 1) / 2);
-    let temp;
     /** starting at left most node at bottom level **/
     for (let i = start; i >= 0; i--) {
         /** Call max heapify at each index to fix each sub heap **/
@@ -92,5 +91,72 @@ function heapSort(maxHeap) {
         result.push(extractMax(maxHeap));
     }
     return result;
-    
 }
+
+class PriorityQueue {
+    constructor(array) {
+        this.array = array;
+    }
+
+    buildMaxHeap() {
+        let start = Math.floor((this.array.length - 1) / 2);
+        for (let i = start; i >= 0; i--) {
+            this.maxHeapify(this.array, i);
+        }
+        return this.array;
+    }
+    maxHeapify(array, index) {
+        let largest = index; // set largest to index
+        let leftIndex = (2 * index) + 1;
+        let rightIndex = (2 * index) + 2;
+        let size = array.length - 1;
+        let temp;
+        /** if left index is in heap and element at left is > than element at root/index **/
+        if (leftIndex <= size && array[leftIndex] > array[largest]) {
+            largest = leftIndex;
+        }
+        /** check if right child is larger that current largest **/
+        if (rightIndex <= size && array[rightIndex] > array[largest]) {
+            largest = rightIndex;
+        }
+        /** if largest is not the index -- swap index and largest **/
+        if (largest !== index) {
+            temp = array[index];
+            array[index] = array[largest];
+            array[largest] = temp;
+            /** heapify the next subTree with largest at root/index **/
+            this.maxHeapify(array, largest);
+        }
+        return array;
+    }
+    extractMax() {
+        let size = Math.floor(this.array.length - 1);
+        /** max is always first index of maxHeap **/
+        let max = this.array[0];
+        /** swap the last element with the 'removed' first element **/
+        this.array[0] = this.array[size];
+        /** decrement the array size to remove the first element **/
+        this.array.length -= 1; 
+        /** run maxHeapify to re-balance the maxHeap **/
+        this.maxHeapify(this.array, 0);
+        /** return max **/
+        return max;
+    }
+    insert(node) {
+        /** add node to left most available position **/
+        this.array.push(node);
+        let start = Math.floor((this.array.length - 1) / 2);
+        /** starting at left most node at bottom level **/
+        for (let i = start; i >= 0; i--) {
+            /** Call max heapify at each index to fix each sub heap **/
+            this.maxHeapify(this.array, i);
+        }
+        return this.array;
+    }
+}
+const test3 = [11,13,5,7,6,12,17,19,28,100,29];
+const heap = new PriorityQueue(test3);
+heap.buildMaxHeap();
+heap.insert(234);
+heap.extractMax();
+console.log(heap);
