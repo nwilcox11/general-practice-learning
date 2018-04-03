ddf2017819296792822d14edc456e337221e25bf
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-04-02 23:57:47 -0400

    changes queue from sll to dll inorder to get O(1) dequeue

--- training/dataStructures/queue.js
+++ training/dataStructures/queue.js
@@ -1,27 +1,51 @@
+/**
+ * FIFO, Items entered at the end of the Queue
+ * **/
+
 class Node {
     constructor(value) {
         this.value = value;
         this.next = null;
+        this.previous = null;
     }
 }
 
 class Queue {
     constructor() {
         this.head = null;
+        this.tail = null;
         this.length = 0;
     }
     enqueue(value) {
         const node = new Node(value);
-        node.next = this.head;
-        this.head = node;
+        /** if there are nodes in the list **/
+        if (this.head) {
+            /** add the new node to the end of this list **/
+            this.tail.next = node;
+            node.previous = this.tail;
+            this.tail = node;
+        } else {
+            this.head = node;
+            this.tail = node;
+        }
         this.length++;
         return this;
     }
     dequeue() {
-        
+        /** if head is null queue is empty, set tail to null **/
+        if(!this.head) {
+            this.tail = null;
+            return;
+        }
+        /** grab the value at the head **/
+        const value = this.head.value;
+        /** set current head to the next value **/
+        this.head = this.head.next;
+        this.length--;
+        return value;
     }
     peek() {
-        
+        return this.head.value;
     }
 }
 
