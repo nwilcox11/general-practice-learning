bc00a50701b3405d0cabbdaf97ccbab0d363a9f3
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-04-18 00:00:13 -0400

    adds comments

--- training/addTwoLists.js
+++ training/addTwoLists.js
@@ -36,6 +36,12 @@ function addTwoNumbers(l1, l2) {
         let num1 = list1 ? list1.val : 0;
         let num2 = list2 ? list2.val : 0;
         let sum = carry + num1 + num2;
+        /** carry can either be a 1 or 0
+         * because the largest possible sum of 
+         * two digits (including the carry) is 9 + 9 + 1 = 19
+         * Since each digit is in the range of 0-9, summing two digits may "overflow". For example 5 + 7 = 12. 
+         * In this case, we set the current digit to 2 and bring over the 1 to the next iteration.
+         *  **/
         carry = Math.floor(sum / 10);
         result.next = new ListNode(sum % 10);
         /** advance result to continue build list **/
@@ -52,3 +58,4 @@ function addTwoNumbers(l1, l2) {
     }
     return head.next;
 }
+addTwoNumbers(input1, input2)
