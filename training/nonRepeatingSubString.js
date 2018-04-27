fb07fe6489b295a2a41ec2f0fc35e83968a631c2
nwilcox11 <wilcox.nicholas.m@gmail.com>
2018-04-26 22:56:14 -0400

    adds some comments explaining the solution

--- training/nonRepeatingSubString.js
+++ training/nonRepeatingSubString.js
@@ -4,29 +4,30 @@ const string3 = 'au'; // 2
 const string4 = "dvdf" // 3 
 const string5 = "pwwkew" // 3
 
+/** 
+ * Window algorithm
+ *  **/
+
 function lengthOfLongestSubstring(s) {
     let map = {};
-    let str = '';
-    let count = 0;
     let n = s.length;
+    let count = 0;
     let i = 0;
     let j = 0;
-
+    /** O(n) where n is the length of the string **/
     while(i < n && j < n ) {
         if (!map[s[i]]) {
             map[s[i]] = true;
             i++;
-            count = count > i - j ? count : i - j
+            /** i and j are the window to check for substring, we keep sliding i until we hit an element we have seen already.
+             * We then increment j and check again by sliding i until we reach the end of the string.  
+             * We keep the count with i - j being the new challenging substring length **/
+            count = count > i - j ? count : i - j // i - j is the new count length
         } else {
+            /** move j, start count over **/
             map[s[j]] = false;
             j++
         }
     }
     return count;
 }
-    
-console.log(lengthOfLongestSubstring(string))
-// console.log(lengthOfLongestSubstring(string2))
-// console.log(lengthOfLongestSubstring(string3))
-// console.log(lengthOfLongestSubstring(string4))
-// console.log(lengthOfLongestSubstring(string5))
